05b918f0aaf6e85f4a3f3afaadf923fb6b21936a
Naoyuki Kumagai <kumagai@uranus>
2010-11-30 11:55:42 +0900

    First import with brand new Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'dd98ced6e097779134e41ba42daf78ca9e1db14940ecd637a5ee2b55708edb69a0cd0d78296bb1b5215b104da31dd50332eb4445c29541b41637c75a5ef9ba4b'
