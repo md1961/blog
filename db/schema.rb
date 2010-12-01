f90ed8e4ca76ed062889dfa9b62918d72cd44dd2
Naoyuki Kumagai <kumagai@fergus>
2010-12-01 11:43:10 +0900

    Created model User
    db/migrate/20101201023955_create_users.rb
    app/models/user.rb
    - Newly created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101130135804) do
+ActiveRecord::Schema.define(:version => 20101201023955) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20101130135804) do
     t.datetime "updated_at"
     t.string   "excerpt"
     t.string   "location"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
