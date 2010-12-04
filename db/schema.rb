25ba25b8db6b97c4ed03c75561d840e2a23e41ff
Naoyuki Kumagai <kumagai@fergus>
2010-12-04 22:56:29 +0900

    Added model Category
    db/migrate/20101204135055_create_categories.rb
    app/models/category.rb
    - Newly created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101201091013) do
+ActiveRecord::Schema.define(:version => 20101204135055) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(:version => 20101201091013) do
     t.string   "excerpt"
     t.string   "location"
     t.integer  "user_id"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
