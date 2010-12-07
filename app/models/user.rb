1c2c517f94767a408d56edb538c2906c449e3b2b
Naoyuki Kumagai <kumagai@uranus>
2010-12-07 17:16:02 +0900

    Added validates :password for :confirmation of model User
    app/models/user.rb
    - Added validates :password for :confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true,
                     :length => {:within => 5..50},
                     :format => {:with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i}
+  validates :password, :confirmation => true
 end
