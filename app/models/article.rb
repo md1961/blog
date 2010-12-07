c211909477bb078de83464eec941e63ae74caf96
Naoyuki Kumagai <kumagai@uranus>
2010-12-07 13:48:58 +0900

    Added named scope to model Article
    app/models/article.rb
    - Added scope :published, :draft, :recent and :where_title.

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,11 @@ class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body , :presence => true
 
+  scope :published  , where("articles.published_at IS NOT NULL")
+  scope :draft      , where("articles.published_at IS     NULL")
+  scope :recent     , lambda { published.where("articles.published_at > ?", 1.week.ago.to_date) }
+  scope :where_title, lambda { |term| where("articles.title LIKE ?", "%#{term}%") }
+
   def long_title
     return "#{title} - #{published_at}"
   end
