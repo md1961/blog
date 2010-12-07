b09b604f225ec85b7684bf451ac220e643f0ee69
Naoyuki Kumagai <kumagai@uranus>
2010-12-07 16:12:36 +0900

    Added :length to validates :email of model User
    app/models/user.rb
    - Added :length to validates :email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :articles, :order => "published_at DESC, title ASC", :dependent => :nullify
   has_many :replies, :through => :articles, :source => :comments
 
-  validates :email, :uniqueness => true
+  validates :email, :uniqueness => true, :length => {:within => 5..50}
 end
